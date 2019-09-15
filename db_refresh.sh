0954b06fe31083f009c7805ce4925d559f906b0e
Justin Parado <justin.parado@dice205.com>
2019-09-15 13:01:23 +0800

    Updated the standard db reset script

--- db_refresh.sh
+++ db_refresh.sh
@@ -3,6 +3,9 @@
 mysql -u root --execute="DROP DATABASE IF EXISTS ust";
 mysql -u root --execute="CREATE DATABASE IF NOT EXISTS ust";
 
+echo pam | sudo -Siu postgres dropdb ust;
+echo pam | sudo -Siu postgres createdb ust;
+
 rm database/database.sqlite;
 touch database/database.sqlite;
 
