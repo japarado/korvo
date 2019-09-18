3c3f2802e85ee1da2b89c7450932fa7dd590eb33
Justin Parado <justin.parado@dice205.com>
2019-09-18 19:20:38 +0800

    Enabled eager loading on event show view

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -91,8 +91,8 @@ class EventController extends Controller
         $role_user = static::getUserRoleInstance();
         if($user->role_id == Config::get('constants.roles.organization'))
         {
-            /* $event = $role_user->events()->find($id)->with('speakers')->with('students')->get(); */
-            $event = $role_user->events()->find($id);
+            $event = $role_user->events()->find($id)->with('speakers')->with('students')->get();
+            /* $event = $role_user->events()->find($id); */
         }
         elseif($user->role_id == Config::get('constants.roles.socc'))
         {
